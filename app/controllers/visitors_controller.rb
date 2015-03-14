9ddaebdaf23b293e21bd11f99a0f656ad5f205b4
Salman Akram <salmanit2012@gmail.com>
2015-03-14 16:06:53 -0400

    Adding flash messages

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -2,8 +2,8 @@ class VisitorsController < ApplicationController
 
 	def new
 		@owner = Owner.new
-		render 'visitors/new', :layout => false
+		# render 'visitors/new'
 		flash.now[:notice] = "Welcome!!!"
-		flash.now[:alert] = "My birthday is soon"
+		flash.now[:alert] = "My birthday is soon, see you at party!"
 	end
 end
